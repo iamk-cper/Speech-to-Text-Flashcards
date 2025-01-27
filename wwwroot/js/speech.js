a8e845017d0a507a7a9993b117bf711f994faa83
iamk-cper <kacper.mycka02@gmail.com>
2025-01-27 15:29:24 +0100

    Template of the project

---
+++ wwwroot/js/speech.js
@@ -0,0 +1,19 @@
+﻿window.speechRecognitionInterop = {
+    startListening: function (dotnetHelper, language) {
+        const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+        recognition.lang = language || 'pl-PL';
+        recognition.continuous = false;
+        recognition.interimResults = false;
+
+        recognition.onresult = function (event) {
+            const transcript = event.results[0][0].transcript;
+            dotnetHelper.invokeMethodAsync('ProcessSpeech', transcript);
+        };
+
+        recognition.onerror = function (event) {
+            console.error('Recognition error:', event.error);
+        };
+
+        recognition.start();
+    }
+};
